58eb30a380e7c48257d97e2efd26d93d7c2ff30e
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-08-12 12:56:53 +0300

    Room_name issue fixed

--- game/models.py
+++ game/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class GameRoom(models.Model):
+    RoomName = models.CharField(unique=True, max_length=15)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.RoomName
+
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+    score = models.IntegerField(default=0)
+    room = models.ForeignKey(GameRoom, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- game/views.py
+++ game/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import FormView
+
+from game.forms import NickNameForm
+
 
 # Create your views here.
 
 def home(request):
+    template_name = ('game.html')
+    return render(request, template_name)
+
+@csrf_exempt
+def game_room(request, room_name=None):
+    if request.method == "POST":
+        game_name = request.POST.get("game_name")
+        username = request.POST.get("username")
+
+        print(f"Game: {game_name}, User: {username}")
+        return redirect(reverse('room', kwargs={'room_name': game_name}))
+
+    return render(request, 'room.html')
+
+def game(request):
     template_name = ('game.html')
     return render(request, template_name)
