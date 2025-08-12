dc94b7dcb89f749535b6b6907460ebffd66ddb12
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-08-12 12:56:53 +0300

    Room_name issue fixed

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import game_room
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('game_room/', game_room, name='game_room'),
+    path('<str:room_name>/', views.game_room, name='room'),
+
 ]
